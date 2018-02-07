a2c77c38cd3fa1a59feda737deb1ff5a132ec4bb
xu <s231680@studenti.polito.it>
2018-02-06 22:48:22 -0600

    add make directories to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd ..
+mkdir ContainerStop
+mkdir ContainerResotre
+cd migrationServer_geni
+mkdir log
+mkdir lookupTable
 echo "start program..."
 mvn verify
 mvn exec:java -Dexec.mainClass=ServerApplication
